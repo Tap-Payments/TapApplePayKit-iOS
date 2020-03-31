1e2cee1361d7089376e3d9abbb9a3f921cbe539c
Tap Payments <o.rabie@tap.company>
2020-03-31 20:01:51 +0200

    1- First commit project structre

---
+++ TapApplePayKit-iOS/TapApplePayKit-Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TapApplePayKit-Example
+//
+//  Created by Osama Rabie on 31/03/2020.
+//  Copyright © 2020 Tap Payments. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TapApplePayKit-iOS/TapApplePayKit-iOS/TapApplePayKit_iOS.h
@@ -0,0 +1,19 @@
+//
+//  TapApplePayKit_iOS.h
+//  TapApplePayKit-iOS
+//
+//  Created by Osama Rabie on 31/03/2020.
+//  Copyright © 2020 Tap Payments. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TapApplePayKit_iOS.
+FOUNDATION_EXPORT double TapApplePayKit_iOSVersionNumber;
+
+//! Project version string for TapApplePayKit_iOS.
+FOUNDATION_EXPORT const unsigned char TapApplePayKit_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TapApplePayKit_iOS/PublicHeader.h>
+
+
