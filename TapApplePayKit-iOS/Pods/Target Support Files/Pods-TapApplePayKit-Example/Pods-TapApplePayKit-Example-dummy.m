ef6ddbb7523ea75e5432a52639cde7c2cbd57f22
Tap Payments <o.rabie@tap.company>
2020-04-02 12:04:50 +0200

    1- Firebase distribution to Apple Pay Kit exxample

---
+++ TapApplePayKit-iOS/Pods/Target Support Files/Pods-TapApplePayKit-Example/Pods-TapApplePayKit-Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TapApplePayKit_Example : NSObject
+@end
+@implementation PodsDummy_Pods_TapApplePayKit_Example
+@end
---
+++ TapApplePayKit-iOS/Pods/Target Support Files/Pods-TapApplePayKit-iOS/Pods-TapApplePayKit-iOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TapApplePayKit_iOS : NSObject
+@end
+@implementation PodsDummy_Pods_TapApplePayKit_iOS
+@end
