50c66bb036c678800d74ce02bb62af781296ea50
Tap Payments <o.rabie@tap.company>
2020-04-02 08:32:50 +0200

    Documentation

--- TapApplePayKit-iOS/TapApplePayKit-iOS/Core/public/Models/TapApplePayRequest.swift
+++ TapApplePayKit-iOS/TapApplePayKit-iOS/Core/public/Models/TapApplePayRequest.swift
@@ -9,16 +9,34 @@
 import Foundation
 import PassKit
 
+/// The class that represents the request details that identefies the transaction
 @objc public class TapApplePayRequest:NSObject {
     
+    /// The country code where the user transacts
     lazy var countryCode:TapCountryCode = .US
+    /// The currency code the transaction has
     lazy var currencyCode:TapCurrencyCode = .USD
-    var paymentNetworks:[TapApplePayPaymentNetwork] = []
+    /// The payment networks you  want to limit the payment to
+    lazy var paymentNetworks:[TapApplePayPaymentNetwork] = []
+    /// What are the items you want to show in the apple pay sheet
     lazy var paymentItems:[PKPaymentSummaryItem] = []
+    /// The total amount you want to collect
     lazy var paymentAmount:Double = 0
+    /// The apple pay merchant identefier
     lazy var merchantID:String = ""
+    
+    /// The actual apple pay request
     internal lazy var appleRequest:PKPaymentRequest = .init()
     
+    /**
+     Creates a Tap Apple Pay request that can be used afterards to make an apple pay request
+     - Parameter countryCode: The country code where the user transacts default .US
+     - Parameter currencyCode: The currency code the transaction has default .USD
+     - Parameter paymentNetworks:  The payment networks you  want to limit the payment to default []
+     - Parameter var paymentItems: What are the items you want to show in the apple pay sheet default  []
+     - Parameter paymentAmount: The total amount you want to collect default 0
+     - Parameter merchantID: The apple pay merchant identefier default ""
+     **/
     public func build(with countryCode:TapCountryCode, paymentNetworks:[TapApplePayPaymentNetwork] = [], paymentItems:[PKPaymentSummaryItem] = [], paymentAmount:Double = 0,currencyCode:TapCurrencyCode = .USD,merchantID:String) {
         self.countryCode = countryCode
         self.paymentNetworks = paymentNetworks
