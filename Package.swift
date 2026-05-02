33fa905b83941242cc2f5dae29e9936eabe9e86b
Muhammad Zahid Imran <33024798+zahidappsgenii@users.noreply.github.com>
2026-05-02 23:56:37 +0500

    updates

--- Package.swift
+++ Package.swift
@@ -14,25 +14,16 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(
-            url: "https://github.com/Tap-Payments/CommonDataModelsKit-iOS.git",
-            from: "1.0.145"
-        ),
-        .package(
-            url: "https://github.com/Tap-Payments/TapNetworkKit-iOS.git",
-            from: "1.0.14"
-        ),
-        .package(
-            url: "https://github.com/Tap-Payments/TapApplicationV2.git",
-            from: "0.0.3"
-        )
+        .package(url: "https://github.com/Tap-Payments/CommonDataModelsKit-iOS.git", from: "1.0.149"),
+        .package(url: "https://github.com/Tap-Payments/TapNetworkKit-iOS.git", from: "1.0.14"),
+        .package(url: "https://github.com/Tap-Payments/TapApplicationV2.git", from: "0.0.3")
     ],
     targets: [
         .target(
             name: "TapApplePayKit-iOS",
             dependencies: [
-                .product(name: "CommonDataModelsKit_iOS", package: "CommonDataModelsKit_iOS"),
-                .product(name: "TapNetworkKit-iOS", package: "TapNetworkKit-iOS"),
+                .product(name: "CommonDataModelsKit_iOS", package: "CommonDataModelsKit-iOS"),
+                .product(name: "TapNetworkKit_iOS", package: "TapNetworkKit-iOS"),
                 .product(name: "TapApplicationV2", package: "TapApplicationV2")
             ],
             path: "TapApplePayKit-iOS/Core",
